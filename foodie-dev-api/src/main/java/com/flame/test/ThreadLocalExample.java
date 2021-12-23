041b6bbff54e4f0d476e6420f49a49eeac4f8c45
laihuibo <994810552@qq.com>
2021-12-23 13:24:05 +0800

    refactor:

--- foodie-dev-api/src/main/java/com/flame/test/ThreadLocalExample.java
+++ foodie-dev-api/src/main/java/com/flame/test/ThreadLocalExample.java
@@ -24,16 +24,16 @@ public class ThreadLocalExample implements Runnable{
 
     @Override
     public void run() {
-        System.out.println("Thread Name= "+Thread.currentThread().getName()+" default Formatter = "+formatter.get().toPattern());
+        System.out.println("Thread Name= "+Thread.currentThread().getName()+" default Formatter = "+FORMATTER.get().toPattern());
         try {
             Thread.sleep(new Random().nextInt(1000));
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         //formatter pattern is changed here by thread, but it won't reflect to other threads
-        formatter.set(new SimpleDateFormat());
+        FORMATTER.set(new SimpleDateFormat());
 
-        System.out.println("Thread Name= "+Thread.currentThread().getName()+" formatter = "+formatter.get().toPattern());
+        System.out.println("Thread Name= "+Thread.currentThread().getName()+" formatter = "+FORMATTER.get().toPattern());
     }
 
 }
